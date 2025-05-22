3981201fd49a2996ab536a7182404cfee37c8cd3
MazenSamehR <samehmazen868@gmail.com>
2025-05-22 23:03:00 +0300

    Updates

---
+++ constants/fileTypes.js
@@ -0,0 +1,9 @@
+export const FileType = {
+    FOLDER: 'folder',
+    DOCUMENT: 'document',
+    SPREADSHEET: 'spreadsheet',
+    PRESENTATION: 'presentation',
+    IMAGE: 'image',
+    VIDEO: 'video',
+    OTHER: 'other'
+  };
